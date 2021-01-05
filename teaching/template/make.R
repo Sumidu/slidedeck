446af778d6aa957600bca9bd124869f0dfd9f139
André Calero Valdez <andrecalerovaldez@gmail.com>
2021-01-05 12:10:31 +0100

    added a simple make file to knit all Rmds

---
+++ teaching/template/make.R
@@ -0,0 +1,19 @@
+# This file runs all test documents
+
+
+library(stringr)
+# run test files
+base_path <- "teaching/template"
+files <- dir(path = base_path, pattern = "*.Rmd")
+
+# remove the readme
+files <- files[!files %in% c("README.Rmd")]
+
+output <- str_replace(files, ".Rmd", ".html")
+output <- paste0("output/", output)
+
+
+for (i in 1:length(files)) {
+  message(paste0("Rendering document: ", files[i]))
+  rmarkdown::render(input = paste0(base_path, "/", files[i]))
+}
